d1142d0706cb43a2ef1dd139481782d027c71053
Jan Banko <41587334+Xxm0dxX@users.noreply.github.com>
2018-12-03 22:52:18 +0100

    Update 03

--- 03/Puzzle3/Program.cs
+++ 03/Puzzle3/Program.cs
@@ -13,25 +13,25 @@ namespace Puzzle3
             var claims = File.ReadAllLines("./input.txt").Select(line =>
             {
                 var data = Regex.Split(line, @"\D+").Skip(1).Select(x => int.Parse(x)).ToList();
-                return (data[0], data[1], data[2], data[3], data[4], new HashSet<int>());
+                return (id: data[0], left: data[1], top: data[2], x: data[3], y: data[4], indexes: new HashSet<int>());
             }).ToList();
-            var sizeX = claims.Max(x => x.Item2) + claims.Max(x => x.Item4);
-            var sizeY = claims.Max(x => x.Item3) + claims.Max(x => x.Item5);
+            var sizeX = claims.Max(x => x.left) + claims.Max(x => x.x);
+            var sizeY = claims.Max(x => x.top) + claims.Max(x => x.y);
             var arr = new int[sizeX * sizeY];
 
             for (int i = 0; i < claims.Count; i++)
             {
-                for (int j = 0; j < claims[i].Item4; j++)
+                for (int j = 0; j < claims[i].x; j++)
                 {
-                    for (int k = 0; k < claims[i].Item5; k++)
+                    for (int k = 0; k < claims[i].y; k++)
                     {
-                        var index = (claims[i].Item3 + k) * sizeY + claims[i].Item2 + j;
+                        var index = (claims[i].top + k) * sizeY + claims[i].left + j;
                         arr[index]++;
-                        claims[i].Item6.Add(index);
+                        claims[i].indexes.Add(index);
                     }
                 }
             }
-            var newT = claims.FirstOrDefault(claim => claim.Item6.Where(x => arr[x] > 1).Count() == 0).Item1;
+            var newT = claims.FirstOrDefault(claim => claim.indexes.Where(x => arr[x] > 1).Count() == 0).id;
             var res = arr.Count(x => x > 1);
 
             Console.WriteLine($"Result (Part 1): {res}");
